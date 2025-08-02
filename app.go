f0fd8a915fd95a36b43bfacf7cc9501b412fbf4a
Vikas Gupta <vikasg619@gmail.com>
2025-08-02 14:10:57 +0530

    added module

--- app.go
+++ app.go
@@ -1,10 +1,9 @@
 package main
 
 import (
- "fmt"
+	"fmt"
 )
 
-func main(){
-  fmt.Println("Hello world!")
+func main() {
+	fmt.Println("Hello world!")
 }
-
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gupta619/golang-projects.git
+
+go 1.24.5
